b18daed65ff3731ea1c2636895eff27b7aeae6ff
jfrez <jonathan.frez@gmail.com>
2025-10-21 12:22:05 -0300

    Restringir acceso de supervisor a seccion1 y subir version

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 if (!defined('APP_VERSION')) {
-    define('APP_VERSION', '1.0.1');
+    define('APP_VERSION', '1.0.2');
 }
 
 // CSRF token handling
